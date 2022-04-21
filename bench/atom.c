a36f08b4c630d73b18a9ede0cf7bce70c628d400
Yuichiro Hanada <yhanada@google.com>
2022-04-21 21:57:38 +0900

    Uprev libxkbcommon to 1.4.0.
    1. Do 'meson config -Denable-x11=false -Denable-wayland=false -Denable-docs=false' to generate config.h
    2. Remove #defines in config.h for non-existing funcs
    3. atom.c is modified to suppress Wreturn-type.
    
    Bug: 228955027
    Test: m
    Change-Id: I278a58f24712b21522e6a14a2a6a3979406b40e9
    (cherry picked from commit 5f097b82a17ab8d7955aa9b3c35dcb11348dc4ed)

--- bench/atom.c
+++ bench/atom.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009 Dan Nicholson
+ * Copyright © 2021 Ran Benita <ran@unusedvar.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -23,41 +23,71 @@
 
 #include "config.h"
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
 
-#include "../test/test.h"
+#include "atom.h"
 #include "bench.h"
+#include "darray.h"
 
-#define BENCHMARK_ITERATIONS 1000
+#define BENCHMARK_ITERATIONS 100
 
 int
-main(int argc, char *argv[])
+main(void)
 {
-    struct xkb_context *ctx;
-    struct xkb_keymap *keymap;
+    FILE *file;
+    char wordbuf[1024];
+    darray(char *) words;
+    char **worditer;
+    struct atom_table *table;
+    xkb_atom_t atom;
+    const char *text;
     struct bench bench;
     char *elapsed;
-    int i;
 
-    ctx = test_get_context(0);
-    assert(ctx);
-
-    xkb_context_set_log_level(ctx, XKB_LOG_LEVEL_CRITICAL);
-    xkb_context_set_log_verbosity(ctx, 0);
+    darray_init(words);
+    file = fopen("/usr/share/dict/words", "rb");
+    if (file == NULL) {
+        perror("/usr/share/dict/words");
+        return -1;
+    }
+    while (fgets(wordbuf, sizeof(wordbuf), file)) {
+        size_t len = strlen(wordbuf);
+        if (len > 0 && wordbuf[len - 1] == '\n')
+            wordbuf[len - 1] = '\0';
+        darray_append(words, strdup(wordbuf));
+    }
+    fclose(file);
 
     bench_start(&bench);
-    for (i = 0; i < BENCHMARK_ITERATIONS; i++) {
-        keymap = test_compile_rules(ctx, "evdev", "evdev", "us", "", "");
-        assert(keymap);
-        xkb_keymap_unref(keymap);
+    for (int i = 0; i < BENCHMARK_ITERATIONS; i++) {
+        table = atom_table_new();
+        assert(table);
+
+        darray_foreach(worditer, words) {
+            atom = atom_intern(table, *worditer, strlen(*worditer) - 1, true);
+            assert(atom != XKB_ATOM_NONE);
+
+            text = atom_text(table, atom);
+            assert(text != NULL);
+        }
+
+        atom_table_free(table);
     }
     bench_stop(&bench);
 
     elapsed = bench_elapsed_str(&bench);
-    fprintf(stderr, "compiled %d keymaps in %ss\n",
+    fprintf(stderr, "%d iterations in %ss\n",
             BENCHMARK_ITERATIONS, elapsed);
     free(elapsed);
 
-    xkb_context_unref(ctx);
+    darray_foreach(worditer, words) {
+        free(*worditer);
+    }
+    darray_free(words);
+
     return 0;
 }
